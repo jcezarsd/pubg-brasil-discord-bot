95af2e3f98ab1ed68d4482df86cf918dddc77123
Julio Cezar Santos Diniz <julio.diniz@gt4w.com.br>
2020-05-26 15:33:01 -0300

    Update os eslint config

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-const Pomocnik = require('./lib/Pomocnik');
+const Pomocnik = require("./lib/Pomocnik");
 
-const { token } = require('../settings.json');
+const { token } = require("../settings.json");
 
 const client = new Pomocnik({
-	prefix: '!',
-	cmdEditing: true,
-	providers: { default: 'rethinkdb' },
-	fetchAllMembers: true,
-	disabledEvents: [
-		'TYPING_START',
-		'RELATIONSHIP_ADD',
-		'RELATIONSHIP_REMOVE',
-		'CHANNEL_PINS_UPDATE',
-		'PRESENCE_UPDATE',
-		'USER_UPDATE',
-		'USER_NOTE_UPDATE'
-	]
+  prefix: "!",
+  cmdEditing: true,
+  providers: { default: "rethinkdb" },
+  fetchAllMembers: true,
+  disabledEvents: [
+    "TYPING_START",
+    "RELATIONSHIP_ADD",
+    "RELATIONSHIP_REMOVE",
+    "CHANNEL_PINS_UPDATE",
+    "PRESENCE_UPDATE",
+    "USER_UPDATE",
+    "USER_NOTE_UPDATE",
+  ],
 });
 
 return client.login(token);
--- src/lib/PubgClient.js
+++ src/lib/PubgClient.js
@@ -1,22 +1,22 @@
-const settings = require('../../settings.json');
+const settings = require("../../settings.json");
 
-const pubgRoyale = require('pubg-royale');
+const pubgRoyale = require("pubg-royale");
 
 module.exports = new pubgRoyale.Client({
-	// Put your api key here
-	key: settings.pubgApiKey,
-  
-	// Default region used for api calls. Defaults to "steam" if omitted.
-	// The region can be set for individual api calls.
-	defaultRegion: pubgRoyale.REGIONS.PC.STEAM,
-  
-	// Specifies ttl in ms for cached objects. Any value ommited defaults to 60 seconds.
-	// Set every value to zero to disable caching
-	cache: { 
-		player: 10 * 1000,
-		playerStats: 10 * 1000,
-		match: 10 * 1000,
-		status: 10 * 1000,
-		seasons: 10 * 1000,
-	},
+  // Put your api key here
+  key: settings.pubgApiKey,
+
+  // Default region used for api calls. Defaults to "steam" if omitted.
+  // The region can be set for individual api calls.
+  defaultRegion: pubgRoyale.REGIONS.PC.STEAM,
+
+  // Specifies ttl in ms for cached objects. Any value ommited defaults to 60 seconds.
+  // Set every value to zero to disable caching
+  cache: {
+    player: 10 * 1000,
+    playerStats: 10 * 1000,
+    match: 10 * 1000,
+    status: 10 * 1000,
+    seasons: 10 * 1000,
+  },
 });
